d04d790c09c797f60da80d4b723277150fcbbf31
DouiriMouheb <douirijet1@gmail.com>
2025-07-10 09:28:21 +0200

    4th stable version

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ async function startServer() {
     }
 
     // Start server
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       logger.info(
         `Server running on port ${PORT} in ${config.server.env} mode`
       );
